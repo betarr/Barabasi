ff1aba2a727188eae9d29accf62ec029ce512792
Martin Sochuliak <martin.sochuliak@gmail.com>
2013-04-13 20:54:38 +0200

    Performance test form degree driven added

---
+++ src/sk/sochuliak/barabasi/runner/PerformanceDegreeDrivenTest.java
@@ -0,0 +1,54 @@
+package sk.sochuliak.barabasi.runner;
+
+import java.util.Date;
+
+import sk.sochuliak.barabasi.network.ArrayNetwork;
+import sk.sochuliak.barabasi.network.MapNetwork;
+import sk.sochuliak.barabasi.network.Network;
+import sk.sochuliak.barabasi.network.NetworkBase;
+import sk.sochuliak.barabasi.network.NodeToNodeNetwork;
+import sk.sochuliak.barabasi.network.ObjectedNetwork;
+
+public class PerformanceDegreeDrivenTest {
+	public static final int NODES = 30000;
+	public static final int EDGES = 2;
+
+	public static void main(String[] args) {
+		PerformanceDegreeDrivenTest.buildArrayNetwork();
+		PerformanceDegreeDrivenTest.buildMapNetwork();
+		PerformanceDegreeDrivenTest.buildNodeToNodeNetwork();
+		PerformanceDegreeDrivenTest.buildObjectedNetwork();
+	}
+	
+	public static void buildArrayNetwork() {
+		long start = new Date().getTime();
+		Network network = ArrayNetwork.buildNetwork(PerformanceDegreeDrivenTest.NODES, PerformanceDegreeDrivenTest.EDGES, NetworkBase.DEGREE_DRIVEN);
+		long end = new Date().getTime();
+		long time = end - start;
+		System.out.println("Network build by " + network.getClass().getName() + " in " + time + " ms");
+	}
+	
+	public static void buildMapNetwork() {
+		long start = new Date().getTime();
+		Network network = MapNetwork.buildNetwork(PerformanceDegreeDrivenTest.NODES, PerformanceDegreeDrivenTest.EDGES, NetworkBase.DEGREE_DRIVEN);
+		long end = new Date().getTime();
+		long time = end - start;
+		System.out.println("Network build by " + network.getClass().getName() + " in " + time + " ms");
+	}
+	
+	public static void buildNodeToNodeNetwork() {
+		long start = new Date().getTime();
+		Network network = NodeToNodeNetwork.buildNetwork(PerformanceDegreeDrivenTest.NODES, PerformanceDegreeDrivenTest.EDGES, NetworkBase.DEGREE_DRIVEN);
+		long end = new Date().getTime();
+		long time = end - start;
+		System.out.println("Network build by " + network.getClass().getName() + " in " + time + " ms");
+	}
+	
+	public static void buildObjectedNetwork() {
+		long start = new Date().getTime();
+		Network network = ObjectedNetwork.buildNetwork(PerformanceDegreeDrivenTest.NODES, PerformanceDegreeDrivenTest.EDGES, NetworkBase.DEGREE_DRIVEN);
+		long end = new Date().getTime();
+		long time = end - start;
+		System.out.println("Network build by " + network.getClass().getName() + " in " + time + " ms");
+	}
+}
