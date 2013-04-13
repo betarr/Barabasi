4f776215c2e3bb1c01254de8af4a18969dc4dbbd
Martin Sochuliak <martin.sochuliak@gmail.com>
2013-04-13 13:35:17 +0200

    ObjectedNetwork implemented

--- src/sk/sochuliak/barabasi/network/ObjectedNetwork.java
+++ src/sk/sochuliak/barabasi/network/ObjectedNetwork.java
@@ -1,10 +1,15 @@
 package sk.sochuliak.barabasi.network;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectedNetwork extends NetworkBase implements Network {
 
 	private List<ObjectedNode> nodes;
+	
+	public ObjectedNetwork() {
+		this.nodes = new ArrayList<ObjectedNode>();
+	}
 	
 	public static Network buildNetwork(int nodesCount, int edgesCount) {
 		// TODO Auto-generated method stub
@@ -13,13 +18,29 @@ public class ObjectedNetwork extends NetworkBase implements Network {
 	
 	@Override
 	public boolean addNode(int nodeId) {
-		// TODO Auto-generated method stub
-		return false;
+		if (this.containsNode(nodeId)) {
+			return false;
+		}
+		
+		this.nodes.add(new ObjectedNode(nodeId));
+		return true;
 	}
 
 	@Override
-	public boolean addEdge(int nodeId1, int nodeId2) {
-		// TODO Auto-generated method stub
+	public boolean addEdge(int nodeId1, int nodeId2) {	
+		if (nodeId1 == nodeId2) {
+			return false;
+		}
+		
+		ObjectedNode node1 = this.getNodeById(nodeId1);
+		ObjectedNode node2 = this.getNodeById(nodeId2);
+		if (node1 != null && node2 != null) {
+			if (!this.isEdgeBetweenNodes(nodeId1, nodeId2)) {
+				node1.addEdge(node2);
+				node2.addEdge(node1);
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -37,8 +58,30 @@ public class ObjectedNetwork extends NetworkBase implements Network {
 
 	@Override
 	public int getNumberOfExistingEdgesBetweenNodes(int[] nodesIds) {
-		// TODO Auto-generated method stub
-		return 0;
+		int numberOfEdges = 0;
+		List<String> countedEdges = new ArrayList<String>();
+		
+		for (int nodeId1 : nodesIds) {
+			for (int nodeId2 : nodesIds) {
+				if (nodeId1 != nodeId2) {
+					
+					String edgeId = "";
+					if (nodeId1 < nodeId2) {
+						edgeId = nodeId1 + "|" + nodeId2;
+					} else {
+						edgeId = nodeId2 + "|" + nodeId1;
+					}
+					
+					if (!countedEdges.contains(edgeId)) {
+						if (this.isEdgeBetweenNodes(nodeId1, nodeId2)) {
+							numberOfEdges++;
+							countedEdges.add(edgeId);
+						}
+					}
+				}
+			}
+		}
+		return numberOfEdges;
 	}
 
 	@Override
@@ -48,32 +91,56 @@ public class ObjectedNetwork extends NetworkBase implements Network {
 
 	@Override
 	public int[] getAdjacentNodesIds(int nodeId) {
-		// TODO Auto-generated method stub
-		return null;
+		ObjectedNode node = this.getNodeById(nodeId);
+		int[] result = new int[node.getAdjacentNodesCount()];
+		if (result.length > 0) {
+			int pointer = 0;
+			List<ObjectedNode> adjacentNodes = node.getAdjacentNodes();
+			for (ObjectedNode adjacentNode : adjacentNodes) {
+				result[pointer] = adjacentNode.getId();
+				pointer++;
+			}
+		}
+		return result;
 	}
 
 	@Override
 	public int getAdjacentNodesCount(int nodeId) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.getNodeById(nodeId).getAdjacentNodesCount();
 	}
 
 	@Override
 	public boolean isEdgeBetweenNodes(int nodeId1, int nodeId2) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.getNodeById(nodeId1).hasEdgeTo(this.getNodeById(nodeId2));
 	}
 
 	@Override
 	public int getNumberOfNodes() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.nodes.size();
 	}
 
 	@Override
 	public boolean containsNode(int nodeId) {
-		// TODO Auto-generated method stub
+		for (ObjectedNode node : this.nodes) {
+			if (node.getId() == nodeId) {
+				return true;
+			}
+		}
 		return false;
+	}
+	
+	/**
+	 * Returns node identified by nodeId.
+	 * @param nodeId Id of node
+	 * @return Node if exists, null otherwise
+	 */
+	private ObjectedNode getNodeById(int nodeId) {
+		for (ObjectedNode node : this.nodes) {
+			if (node.getId() == nodeId) {
+				return node;
+			}
+		}
+		return null;
 	}
 
 }
